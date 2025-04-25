a13ee274371ba22a7797c4c2fd7cc3296bae9576
rafiramzi <79634000+rafiramzi@users.noreply.github.com>
2025-04-25 19:09:47 +0700

    GoWave MVC

--- main.go
+++ main.go
@@ -7,22 +7,5 @@ import (
 
 
 func main() {
-	// if len(os.Args) >= 2 && os.Args[1] == "generate" {
-	// 	generate()
-	// 	return
-	// }
-
 	utils.Boot();
 }
-
-
-// func generate() {
-// 	if len(os.Args) < 4 {
-// 		log.Fatalf("GoWave : Invalid arguments.")
-// 	}
-
-// 	folderName := os.Args[2]
-// 	fileName := os.Args[3]
-
-// 	utils.Generate(folderName, fileName)
-// }
