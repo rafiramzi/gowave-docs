7fb453fa55f2580c0de97d3e279f97f7a13374ea
rafiramzi <79634000+rafiramzi@users.noreply.github.com>
2025-04-25 19:09:18 +0700

    GoWave MVC

--- main.go
+++ main.go
@@ -1,67 +1,28 @@
 package main
 
 import (
-	"log"
-	"os"
 
-	"gowave/database"
-	"gowave/middlewares"
-	"gowave/routes"
 	"gowave/utils"
-
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
 )
 
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	if dbUser == "" || dbPassword == "" || dbName == "" {
-		log.Println("⚠️ Skipping database connection: missing DB_USER, DB_PASSWORD, or DB_NAME")
-	} else {
-		log.Println("DB_NAME:", dbName)
+	// if len(os.Args) >= 2 && os.Args[1] == "generate" {
+	// 	generate()
+	// 	return
+	// }
 
-		db, err := database.Init()
-		if err != nil {
-			log.Fatalf("Could not connect to the database: %v", err)
-		}
-		e := echo.New()
-		e.Use(middlewares.ResponseMiddleware)
-		routes.RegisterRoutes(e, db)
-		port := os.Getenv("APP_PORT")
-		if port == "" {
-			port = "8080"
-		}
-		e.Logger.Fatal(e.Start(":" + port))
-		return
-	}
-	e := echo.New()
-	e.Renderer = utils.NewRenderer("views/*.html")
-	e.Static("/static", "static")
-
-	e.Use(middlewares.ResponseMiddleware)
-	routes.RegisterRoutes(e, nil) 
-	port := os.Getenv("APP_PORT")
-	if port == "" {
-		port = "8080"
-	}
-	e.Logger.Fatal(e.Start(":" + port))
+	utils.Boot();
 }
 
 
-func generate() {
-	if len(os.Args) < 4 {
-		log.Fatalf("GoWave : Invalid arguments.")
-	}
+// func generate() {
+// 	if len(os.Args) < 4 {
+// 		log.Fatalf("GoWave : Invalid arguments.")
+// 	}
 
-	folderName := os.Args[2]
-	fileName := os.Args[3]
+// 	folderName := os.Args[2]
+// 	fileName := os.Args[3]
 
-	utils.Generate(folderName, fileName)
-}
+// 	utils.Generate(folderName, fileName)
+// }
